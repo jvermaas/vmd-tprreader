aff9c8a2713289699c31494e48a88b1861c007f0
Josh Vermaas <vermaasj@msu.edu>
2023-04-06 18:10:28 -0400

    Drop unused file

--- ffread.h
+++
@@ -1,7 +0,0 @@
-/*
-This is the header to read in the force field parameters encoded in a tpr file.
-VMD does not need these per se, or know what to do with them, but because
-these are included *before* the positions that we do want, we need a way of reading
-past them.
-*/
-
